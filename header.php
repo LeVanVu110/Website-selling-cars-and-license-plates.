cd2f37af9377cdb3c6d3b6b84ce0133ebe88ca59
LeVanVu1110 <levanvu111025@gmail.com>
2026-02-10 13:09:40 +0700

    create file Luxury_Cars.php, add link header

--- header.php
+++ header.php
@@ -164,7 +164,7 @@
             <a href="Plate_warehouse.php" class="nav-item text-gray-400 text-[10px] font-bold tracking-[0.3em] hover:text-white transition-all uppercase">Kho biển số</a>
             <a href="Auction.php" class="nav-item text-gray-400 text-[10px] font-bold tracking-[0.3em] hover:text-white transition-all uppercase">Đấu giá</a>
             <a href="#" class="nav-item text-gray-400 text-[10px] font-bold tracking-[0.3em] hover:text-white transition-all uppercase">Xe sang</a>
-            <a href="#" class="nav-item text-gray-400 text-[10px] font-bold tracking-[0.3em] hover:text-white transition-all uppercase">Tin Tức</a>
+            <a href="Luxury_Cars.php" class="nav-item text-gray-400 text-[10px] font-bold tracking-[0.3em] hover:text-white transition-all uppercase">Tin Tức</a>
         </nav>
 
         <div class="flex items-center gap-3 md:gap-6">
@@ -220,7 +220,7 @@
         <a href="Plate_warehouse.php" class="drawer-item font-playfair text-3xl gold-text italic">Kho Biển Số</a>
         <a href="Auction.php" class="drawer-item font-playfair text-3xl gold-text italic">Đấu Giá</a>
         <a href="#" class="drawer-item font-playfair text-3xl gold-text italic">Xe Sang</a>
-        <a href="#" class="drawer-item font-playfair text-3xl gold-text italic">Tin Tức</a>
+        <a href="Luxury_Cars.php" class="drawer-item font-playfair text-3xl gold-text italic">Tin Tức</a>
         <button class="vip-card px-10 py-3 rounded-full text-sm font-bold uppercase tracking-widest mt-4">Đặc Quyền VIP</button>
     </div>
 
