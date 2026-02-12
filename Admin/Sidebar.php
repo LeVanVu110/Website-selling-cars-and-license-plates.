e7b9351ac0915e6f8156f4197184b14da909ecd3
LeVanVu1110 <levanvu111025@gmail.com>
2026-02-12 13:56:19 +0700

    ẩn quản lý xe

--- Admin/Sidebar.php
+++ Admin/Sidebar.php
@@ -151,10 +151,10 @@ $current_page = basename($_SERVER['PHP_SELF']);
                         <div class="min-w-[32px] flex justify-center items-center"><i class="ri-hammer-line text-xl"></i></div>
                         <span class="text-[11px] font-medium tracking-widest ml-4 whitespace-nowrap menu-label uppercase">Phiên đấu giá</span>
                     </a>
-                    <a href="#" class="menu-item flex items-center h-12 px-3 rounded-lg text-gray-400 hover:text-white transition-all group/item">
+                    <!-- <a href="#" class="menu-item flex items-center h-12 px-3 rounded-lg text-gray-400 hover:text-white transition-all group/item">
                         <div class="min-w-[32px] flex justify-center items-center"><i class="ri-roadster-line text-xl"></i></div>
                         <span class="text-[11px] font-medium tracking-widest ml-4 whitespace-nowrap menu-label uppercase">Quản lý xe</span>
-                    </a>
+                    </a> -->
                     <a href="News-management.php" class="menu-item flex items-center h-12 px-3 rounded-lg text-gray-400 hover:text-white transition-all group/item <?= ($current_page == 'News-management.php') ? 'active' : '' ?>">
                         <div class="min-w-[32px] flex justify-center items-center"><i class="ri-newspaper-line text-xl"></i></div>
                         <span class="text-[11px] font-medium tracking-widest ml-4 whitespace-nowrap menu-label uppercase">Tin tức</span>
